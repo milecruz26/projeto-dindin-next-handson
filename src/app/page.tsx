2f42a976b89b2c629e958beb609745523cdf576a
Tayanna Amorim <amorim.tayanna@gmail.com>
2024-02-19 12:00:55 -0300

    :sparkles: feat: component input e form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,49 @@
 import Button from "@/components/button/button";
-import Form from "@/components/form/Form";
+import ContainerForm from "@/components/containerForm";
+import Input from "@/components/input/input";
 import Navbar from "@/components/navbar/Navbar";
 
 export default function Home() {
   return (
-    <main className="min-h-screen p-10 bg-[url('../assets/background.svg')] bg-clip-padding bg-no-repeat  bg-cover bg-center">
+    <main className="min-h-screen  py-10 px-20   bg-[url('../assets/background.svg')] bg-clip-padding bg-no-repeat  bg-cover bg-center">
       <Navbar />
-      <div className="p-0.5 flex items-center">
-        <div className="flex-1 flex flex-col gap-7">
-          <h1 className="font-bold text-5xl text-white">Controle suas <span className="text-purple">finanças</span>, sem planilha chata.</h1>
+      <div className="grid grid-cols-2 items-center justify-center">
+        <div className="flex col-1 flex-col gap-7">
+          <h1 className="font-bold text-5xl text-white">
+            Controle suas <span className="text-purple">finanças</span>,<br /> sem
+            planilha chata.
+          </h1>
           <p className="text-white text-2xl">
             Organizar as suas finanças nunca foi tão fácil, com o DINDIN, você
             tem tudo num único lugar e em um clique de distância.
           </p>
-          <Button text="Cadastre-se"/>
+          <div className="max-w-60">
+          <Button text="Cadastre-se" />
+          </div>
+          
         </div>
-        <Form />
+        <div className="ml-auto">
+        <ContainerForm title="Login">
+          <Input
+            id="email"
+            name="email"
+            type="text"
+            autoComplete="email"
+            label="E-mail"
+          />
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            autoComplete="password"
+            label="Senha"
+          />
+          <div>
+            <Button text="Entrar" type="submit"/>
+          </div>
+        </ContainerForm>
+        </div>
+        
       </div>
     </main>
   );
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -3,8 +3,9 @@ import React from "react";
 interface ButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   text: string;
   type?: "submit" | "button";
+
 }
 
 export default function Button({text, type} : ButtonProps) {
-  return <button type={type} className="bg-purple text-white py-3 rounded-md min-w-72 flex-0">{text}</button>;
+  return <button type={type} className="bg-purple text-white py-3 rounded-md  w-full">{text}</button>;
 }
